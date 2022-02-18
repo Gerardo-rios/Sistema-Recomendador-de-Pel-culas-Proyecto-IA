5ecdb74b1534f76dd10b1bc2247a51685e548790
israel leon <israel.leon@ioet.com>
2022-02-18 00:04:35 -0500

    endpoint

--- SRP/urls.py
+++ SRP/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import model
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homePage, name="home_page"),
+    path('recomendacion/', model.recomendation, name="recomendation"),
 ] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
